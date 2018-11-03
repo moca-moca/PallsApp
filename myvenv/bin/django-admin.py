7e1f365cd187b3d0458f0e8e9a49eb5e162dd0c4
Shimozono Misato <>
2018-11-03 14:47:09 +0900

    first commit

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/osato/projects/private/python/django_tutorial/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
